d54fc720893d6965fbbd472af90f6b9123ce99c9
chemerinskaya <chemerinskaya@tehnokod.com>
2024-04-20 23:41:50 +0300

    fix: доработаны тесты по InMemoryHistoryManagerTest и внесены исправления в методы InMemoryHistoryManager

--- src/history/InMemoryHistoryManager.java
+++ src/history/InMemoryHistoryManager.java
@@ -13,65 +13,61 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void add(Task task) {
-        Node node;
+        removeNode(task.getId());
+        linkLast(task);
+    }
+
+    @Override
+    public void remove(int id) {
+        removeNode(id);
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return getTasks();
+    }
+
+    private void removeNode(int id) {
+        Node node = map.get(id);
+        if (node != null) {
+            if (node.prev == null && node.next == null) {
+                this.tail = null;
+                this.head = null;
+            } else if (node.prev != null && node.next != null) {
+                node.prev.next = node.next;
+                node.next.prev = node.prev;
+            } else if (node.prev == null) {
+                this.head = node.next;
+                this.head.prev = null;
+            } else if (node.next == null) {
+                this.tail = node.prev;
+                this.tail.next = null;
+            }
+            map.remove(id);
+        }
+    }
+
+    private void linkLast(Task task) {
+        Node node = new Node(task, null, tail);
         if (head == null) {
-            node = new Node(task, null,null);
             head = node;
         } else {
-            // ищем дубликат, если есть - удаляем
-            remove(task.getId());
-
-            node = new Node(task, null, tail);
             tail.next = node;
         }
         tail = node;
         map.put(task.getId(), node);
     }
 
-    @Override
-    public void remove(int id) {
-        System.out.println(map);
-        System.out.println(map.containsKey(id));
-
-        if (map.containsKey(id)) {
-
-            removeNode(id);
-        }
-    }
-
-    private void removeNode(int id) {
-        Node node = map.get(id);
-        if (node.prev == null && node.next == null) {
-            this.tail = null;
-            this.head = null;
-        } else if (node.prev != null && node.next != null) {
-            node.prev.next = node.next;
-            node.next.prev = node.prev;
-        } else if (node.prev == null) {
-            this.head = node.next;
-            this.head.prev = null;
-        } else if (node.next == null) {
-            this.tail = node.prev;
-            this.tail.next = null;
-        }
-        map.remove(id);
-    }
-
-    private void linkLast(Task task) {
-        add(task);
-    }
-
     private List<Task> getTasks() {
         List<Task> newList = new ArrayList<>();
-        for (Node node : map.values()) {
-            newList.add(node.data);
-        }
-        return newList;
-    }
 
-    @Override
-    public List<Task> getHistory() {
-        return getTasks();
+        Node current = this.head;
+        while (current != null) {
+            newList.add(current.data);
+            current = current.next;
+        }
+
+        return newList;
     }
 
     private static class Node {
@@ -84,6 +80,8 @@ public class InMemoryHistoryManager implements HistoryManager {
             this.next = next;
             this.prev = prev;
         }
+
     }
+
 }
 
--- test/history/InMemoryHistoryManagerTest.java
+++ test/history/InMemoryHistoryManagerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import tasks.Task;
 
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,8 +13,8 @@ class InMemoryHistoryManagerTest {
 
     HistoryManager historyManager;
     Task task1;
-    Task task6;
-    Task task11;
+    Task task2;
+    Task task3;
     int numberId = 0;
 
     @BeforeEach
@@ -24,38 +23,14 @@ class InMemoryHistoryManagerTest {
 
         task1 = new Task("Таск1","1");
         task1.setId(generateId());
-        Task task2 = new Task("Таск2","2");
+        task2 = new Task("Таск2","2");
         task2.setId(generateId());
-        Task task3 = new Task("Таск3","3");
+        task3 = new Task("Таск3","3");
         task3.setId(generateId());
-        Task task4 = new Task("Таск4","4");
-        task4.setId(generateId());
-        Task task5 = new Task("Таск5","5");
-        task5.setId(generateId());
-        task6 = new Task("Таск6","6");
-        task6.setId(generateId());
-        Task task7 = new Task("Таск7","7");
-        task7.setId(generateId());
-        Task task8 = new Task("Таск8","8");
-        task8.setId(generateId());
-        Task task9 = new Task("Таск9","9");
-        task9.setId(generateId());
-        Task task10 = new Task("Таск10","10");
-        task10.setId(generateId());
-        task11 = new Task("Таск11","11");
-        task11.setId(generateId());
 
         historyManager.add(task1);
         historyManager.add(task2);
         historyManager.add(task3);
-        historyManager.add(task4);
-        historyManager.add(task5);
-        historyManager.add(task6);
-        historyManager.add(task7);
-        historyManager.add(task8);
-        historyManager.add(task9);
-        historyManager.add(task10);
-        historyManager.add(task11);
     }
 
     private int generateId() {
@@ -73,17 +48,17 @@ class InMemoryHistoryManagerTest {
         assertEquals(task1.getStatus(), currentTask.getStatus());
     }
 
-    // проверяет что больше 10 тасков добавятся в HistoryManager
+    // проверяет что сохранит 3 таска
     @Test
-    public void save10TasksInHistoryManager() {
-        assertEquals(historyManager.getHistory().size(), 11);
+    public void save3TasksInHistoryManager() {
+        assertEquals(historyManager.getHistory().size(), 3);
     }
 
     // при вызове remove удаляет таск и размер на 1 меньше
     @Test
-    public void deleteCurrentTaskAndSize10() {
+    public void deleteCurrentTaskAndSize3() {
         historyManager.remove(0);
-        assertEquals(historyManager.getHistory().size(), 10);
+        assertEquals(historyManager.getHistory().size(), 2);
     }
 
     // проверка на удаление из начала истории
@@ -93,27 +68,30 @@ class InMemoryHistoryManagerTest {
 
         List<Task> history = historyManager.getHistory();
 
-        assertFalse(history.contains(task1));
+        assertTrue(history.contains(task2));
+        assertTrue(history.contains(task3));
     }
 
     // проверка на удаление из середины истории
     @Test
     public void deletingFromMiddleOfHistory() {
-        historyManager.remove(5);
+        historyManager.remove(1);
 
         List<Task> history = historyManager.getHistory();
 
-        assertFalse(history.contains(task6));
+        assertTrue(history.contains(task1));
+        assertTrue(history.contains(task3));
     }
 
     // проверка на удаление с конца истории
     @Test
     public void deletingFromEndOfHistory() {
-        historyManager.remove(10);
+        historyManager.remove(2);
 
         List<Task> history = historyManager.getHistory();
 
-        assertFalse(history.contains(task11));
+        assertTrue(history.contains(task1));
+        assertTrue(history.contains(task2));
     }
 
 
